fa35604009cab9aa4dc7433b6f26e906d948c994
saso <krepek.saso1@gmail.com>
2024-06-07 14:11:30 +0200

    testni charti

--- src/pages/dashboard/dashboard.tsx
+++ src/pages/dashboard/dashboard.tsx
@@ -1,8 +1,20 @@
+import ChartOne from "./charts/ChartOne";
+import ChartThree from "./charts/ChartThree";
+import ChartTwo from "./charts/ChartTwo";
+
 export default function Dashboard() {
     return (
         <div>
             <h1>Dashboard</h1>
             <p>Welcome to the dashboard!</p>
+            <div className="grid grid-cols-12 gap-4 md:gap-6 2xl:gap-7.5">
+
+            <ChartOne />
+            <br />
+            <ChartTwo />
+            <br />
+            <ChartThree />
+            </div>
         </div>
     );
 }
