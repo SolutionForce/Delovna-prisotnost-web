f62ff8929a087b6d6755eea4b9dc5a778e9a0160
LahT <teo.lah@student.um.si>
2024-06-28 23:42:00 +0200

    Ureditev API ključev

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,14 +3,14 @@ import {getAuth} from "firebase/auth";
 import {getFirestore} from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBjOKv_555D1UkEVnuS2kmSbNadVZjnvQA",
-  authDomain: "rvir-1e34e.firebaseapp.com",
-  databaseURL: "https://rvir-1e34e-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "rvir-1e34e",
-  storageBucket: "rvir-1e34e.appspot.com",
-  messagingSenderId: "557385771713",
-  appId: "1:557385771713:web:ccd83cc0e1e8b84bb90240",
-  measurementId: "G-LMLP8LTYC2"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 // Initialize Firebase
 export const firebaseApp = initializeApp(firebaseConfig);
